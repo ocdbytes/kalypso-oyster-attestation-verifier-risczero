ac21218f0318cf3e2f3fe1bff8365addbf3c75da
ocdbytes <arunjangra1001@gmail.com>
2025-08-14 18:31:21 +0530

    fix : docker

--- methods/build.rs
+++ methods/build.rs
@@ -1,21 +1,29 @@
-// use std::collections::HashMap;
-// use std::path::Path;
-
-// use risc0_build::{DockerOptions, GuestOptions};
+use risc0_build::{embed_methods_with_options, DockerOptionsBuilder, GuestOptionsBuilder};
 
 fn main() {
-    // let mut options = HashMap::new();
-    // options.insert(
-    //     "guest",
-    //     GuestOptions {
-    //         features: vec![],
-    //         use_docker: Some(DockerOptions {
-    //             root_dir: std::env::current_dir()
-    //                 .unwrap()
-    //                 .parent()
-    //                 .map(Path::to_path_buf),
-    //         }),
-    //     },
-    // );
-    risc0_build::embed_methods();
+    let docker_options = DockerOptionsBuilder::default()
+        .root_dir(std::env::current_dir().unwrap().parent().unwrap())
+        .build()
+        .unwrap();
+    let guest_options = GuestOptionsBuilder::default()
+        .use_docker(docker_options)
+        .build()
+        .unwrap();
+
+    println!("guest_options: {:?}", guest_options);
+    let built_guests = embed_methods_with_options(std::collections::HashMap::from([(
+        "guest",
+        guest_options,
+    )]));
+    println!("built_guests: {:?}", built_guests);
+    let image_id = built_guests[0].image_id;
+
+    // this errs if the dir already exists, so we don't handle an error.
+    let _ = std::fs::create_dir("./risc0/out");
+
+    std::fs::write(
+        "./risc0/out/riscv32im-risc0-vk",
+        format!("0x{}\n", hex::encode(image_id.as_bytes())),
+    )
+    .expect("could not write Risc0 vk to file");
 }
